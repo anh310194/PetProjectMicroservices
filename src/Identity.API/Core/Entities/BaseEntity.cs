c4320c8c4a603ebd3f89619724efbc923e9612c4
Anh Nguyen <anh.nguyen@gigarion.com>
2024-01-24 14:51:55 +0700

    restructure source code

---
+++ src/Identity.API/Core/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+namespace Identity.Domain.Common;
+public abstract class BaseEntity
+{
+    public long Id { get; set; }
+    public DateTime CreatedTime { get; set; }
+    public long CreatedBy { get; set; }
+    public DateTime? UpdatedTime { get; set; }
+    public long? UpdatedBy { get; set; }
+    public byte[]? RowVersion { get; set; }
+}
