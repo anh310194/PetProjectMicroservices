ab04d8584dab08a30ff1c7682fc8870f52d18175
Anh Nguyen <anh.nguyen@gigarion.com>
2024-01-31 13:49:08 +0700

    add RoleFeature

---
+++ src/Identity.API/Core/Entities/RoleFeature.cs
@@ -0,0 +1,10 @@
+
+using Identity.API.Core;
+using Identity.API.Core.Entities.BaseEntity;
+
+public class RoleFeature : BaseEntity
+{
+    public required int RoleId { get; set; }
+    public required int FeatureId { get; set; }
+    public bool IsDeleted { get; set; }
+}
