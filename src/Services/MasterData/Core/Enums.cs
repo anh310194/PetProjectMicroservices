877ab5513bbca6753fc88ab2c739596f6ad4e981
anh310194 <anh310194@gmail.com>
2024-03-07 14:19:07 +0700

    remove some projects do not use

--- src/Services/MasterData/Core/Enums.cs
+++
@@ -1,7 +0,0 @@
-namespace Core;
-
-public enum EnumStatus
-{
-    Activate = 1,
-    InActivate = 0
-}
--- src/Services/MasterData/Core/Interfaces/Persistence/IUnitOfWork.cs
+++
@@ -1,9 +0,0 @@
-namespace Core.Interfaces.Persistence;
-
-public interface IUnitOfWork : IDisposable
-{
-    Task<int> SaveChangesAsync();
-    int SaveChanges();
-    IRepository<Country> CountryRepository { get; }
-    IRepository<State> StateRepository { get; }
-}
